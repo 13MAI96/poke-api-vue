67e01fdabedb4224e4b7436a6d85b6b8bcbbbc33
Marcelo Iglesias - Linux A515 <marceloagustiniglesias@gmail.com>
2025-10-06 19:48:08 -0300

    Added search component

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -2,6 +2,7 @@
 import { inject, onMounted } from 'vue'
 import { pokeApi } from '../services/api'
 import BottomBar from '@/components/BottomBar.vue'
+import Search from '@/components/Search.vue'
 
 const { toggleLoading } = inject('loading')
 let list = []
@@ -13,18 +14,30 @@ onMounted(() => {
 </script>
 
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
+  <div class="home-container">
+    <Search></Search>
+    <div class="about">
+      <h1>This is an about page</h1>
+    </div>
   </div>
   <BottomBar></BottomBar>
 </template>
 
 <style>
-@media (min-width: 1024px) {
-  .about {
-    min-height: 100vh;
-    display: flex;
-    align-items: center;
-  }
+.about {
+  height: calc(100% - 4rem - 50px);
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  overflow-y: scroll;
+}
+.home-container {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  width: 90%;
+  height: calc(100% - 80px);
+  box-sizing: border-box;
+  padding-top: 2rem;
 }
 </style>
